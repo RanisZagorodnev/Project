0c955ebe7c1873c010d7bdb368a144fc3d7faf71
Ranis <aureolbeta@gmail.com>
2026-02-22 22:16:05 +0300

    Add docstrings to functions

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,10 @@ from masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(text: str) -> str:
+    """
+    Принимает строку с типом и номером карты или счета
+    и возвращает строку с замаскированным номером.
+    """
     *names, number = text.split()
     if len(number) == 20:
         return f"Счет {get_mask_account(number)}"
@@ -10,9 +14,12 @@ def mask_account_card(text: str) -> str:
 
 
 def get_date(date: str) -> str:
+    """
+    Принимает строку с датой в формате YYYY-MM-DD...
+    и возвращает дату в формате DD.MM.YYYY.
+    """
     year, month, day = date[:10].split("-")
     return f"{day}.{month}.{year}"
-
 
 print(get_date("2026-03-11T02:26:18.671407"))
 
